cdc486d6a042bfd5d740cc8d037fe25cd97ac67a
ahm4dd <ahmed.emad.edu@gmail.com>
2025-05-20 23:41:21 +0300

    Used ignore and deleted global.d.ts

--- index.ts
+++ index.ts
@@ -2,6 +2,7 @@ import { log, chats } from "./chats.js";
 const button = document.getElementById("enable-button")!;
 
 button.addEventListener("click", () => {
+  // @ts-ignore
   window.supportAI.enableAutoReply();
 });
 
